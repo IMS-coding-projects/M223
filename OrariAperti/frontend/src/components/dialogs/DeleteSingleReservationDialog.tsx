bf92d1e94593f4d84a25158290a6c45baa216e64
An0n-01 <206556099+an0n-01@users.noreply.github.com>
2025-06-16 17:26:12 +0200

    Enhances reservation management and UI interactions
    Refactors reservation editing and deletion logic, introducing a reusable confirmation dialog for deletions. Adds dynamic room selection and feature badges to improve user experience. Implements private/public key handling for access control.
    
    Fixes room ID handling during reservation updates and optimizes error feedback. Improves code readability and consistency by reorganizing imports and cleaning up redundant logic.

---
+++ OrariAperti/frontend/src/components/dialogs/DeleteSingleReservationDialog.tsx
@@ -0,0 +1,39 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import {Trash2} from "lucide-react";
+
+export default function DeleteSingleReservationDialog({ onDelete, deleting }: { onDelete: () => void; deleting: boolean }) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" className={"w-1/2"}><Trash2/>Delete</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Delete Reservation</AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you want to delete this reservation? This action cannot be undone.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button variant="destructive" onClick={onDelete} disabled={deleting}>
+              {deleting ? "Deleting..." : <><Trash2/>Delete</> }
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
